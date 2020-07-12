9dc632216441376256f8fee540af476473c05125
Nick Pisarski <nick.pisarski@gmail.com>
2020-07-11 20:07:13 -0500

    updated settings and changed main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #!v-env/bin/python
-import logging
-from os import path
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="(%(asctime)s) %(name)s:  %(message)s",
-    datefmt="%m.%d.%y@%H:%M:%S",
-)
+# Uncomment for Logging
+# import logging
+# from os import path
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="(%(asctime)s) %(name)s:  %(message)s",
+#     datefmt="%m.%d.%y@%H:%M:%S",
+# )
 
-log = logging.getLogger(path.basename(__file__))
-def main():
-    log.info('It works')
+# log = logging.getLogger(path.basename(__file__))
+
 
 if __name__ == "__main__":
-    main()
+    print('It works!')
